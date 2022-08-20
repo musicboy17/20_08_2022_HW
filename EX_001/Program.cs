9313287d6e61e83216edf1c0fede9316403c1729
musicboy17 <pratham@yandex.ru>
2022-08-20 20:24:10 +0300

    First file_002

--- EX_001/Program.cs
+++ EX_001/Program.cs
@@ -3,3 +3,18 @@
 // чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива size: ");
+int size = Convert.ToInt32(Console.ReadLine()); 
+int [] array = new int [size];
+
+int index = 0;
+int length = array.Length;
+
+while (index < length)
+{
+    array[index] = new Random().Next(100, 1000);
+    index++;
+}
+
+
