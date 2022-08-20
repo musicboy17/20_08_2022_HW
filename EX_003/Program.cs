3c900a02ee480e6ba7a3bfc05f90170e6cb09951
musicboy17 <pratham@yandex.ru>
2022-08-20 22:23:46 +0300

    Tird file

---
+++ EX_003/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу 
+// между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+Console.WriteLine("Введите размер массива size: ");
+int size = Convert.ToInt32(Console.ReadLine()); 
+double [] array = new double [size];
+
+int index = 0;
+int length = array.Length;
+
+while (index < length)
+{
+    array[index] = new Random().Next(1, 100);
+    index++;
+}
+
+Console.WriteLine($"[{String.Join("; ", array)}]");
+
+double minElement = array[0];
+double maxElement = array[0];
+
+int count = array.Length;
+
+for (int i = 0; i < count; i++)
+{
+    if (array[i] < minElement)
+    minElement = array[i];
+    if (array[i] > maxElement)
+    maxElement = array[i];    
+    
+}
+
+double diff = 0;
+diff = maxElement - minElement;
+
+Console.WriteLine($"Минимальный элемент - {minElement} ");
+
+Console.WriteLine($"Максимальный элемент - {maxElement} ");
+
+Console.WriteLine($"Разница между максимальным и минимальным элементами - {diff} ");
+
